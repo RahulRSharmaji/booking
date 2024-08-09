c69b8694aa9f158aadad60c14e95d406c50bbe40
RahulRSharmaji <rahul1998sharmaji@gmail.com>
2024-08-09 23:53:44 +0530

    created components

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+function Home(){
+    return (
+        <>
+            <Header/>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <Footer/>
+        </>
+    )
+}
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import image1 from './images/download.jpeg'
+// import image1 from './images/download.jpeg'
 import './style.css';
 // var date = new Date()
 // var hour = date.getHours()
@@ -12,36 +12,37 @@ import './style.css';
 // } else if(hour>4){
 //     message = "Evening"
 // }
-let style={}
-var marks = prompt('Enter Marks');
-var message = "";
-if (marks <= 40) {
-    message = "Fail";
-    style.color="red";
-} else if (marks > 40 && marks < 50) {
-    message = "Pass with Third division";
-    style.color="green";
-} else if (marks >= 50 && marks < 60) {
-    message = "Pass with Second division";
-    style.color="green";
-} else if (marks >= 60 && marks < 70) {
-    message = "Pass with First division";
-    style.color="green";
-} else if (marks >= 70 && marks <= 100) {
-    message = "Pass with Distinction";
-    style.color="green";
-} else {
-    message = "Invalid Entry";
-    style.color="red";
-}
-
+// let style={}
+// var marks = prompt('Enter Marks');
+// var message = "";
+// if (marks <= 40) {
+//     message = "Fail";
+//     style.color="red";
+// } else if (marks > 40 && marks < 50) {
+//     message = "Pass with Third division";
+//     style.color="green";
+// } else if (marks >= 50 && marks < 60) {
+//     message = "Pass with Second division";
+//     style.color="green";
+// } else if (marks >= 60 && marks < 70) {
+//     message = "Pass with First division";
+//     style.color="green";
+// } else if (marks >= 70 && marks <= 100) {
+//     message = "Pass with Distinction";
+//     style.color="green";
+// } else {
+//     message = "Invalid Entry";
+//     style.color="red";
+// }
+import Home from './Home';
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <>  {/* Wrap elements in <Fragment> to group them together in situations where you need a single element. Grouping elements in Fragment has no effect on the resulting DOM; it is the same as if the elements were not grouped. The empty JSX tag <></> is shorthand for <Fragment></Fragment> in most cases.  */}
-        <h1>Welcome to React world</h1>   
+        {/* <h1>Welcome to React world</h1>   
         <h1 className="text">Welcome to React</h1>   
-        <h1 style={style}>{message}</h1>
+        <h1 className="m-auto" style={style}>{message}</h1>
         <img src={image1} alt="image1" />
-        <img src="logo192.png" alt="logo" />
+        <img src="logo192.png" alt="logo" /> */}
+        <Home/>
     </>
 );
